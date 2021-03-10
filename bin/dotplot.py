ef09122bab2267891619b24f03b84690b6b00b2e
Oleg <brezitskyyoleg@gmail.com>
2021-03-10 09:25:00 +0100

    minor change

--- bin/dotplot.py
+++ bin/dotplot.py
@@ -10,19 +10,19 @@ input = np.loadtxt(sys.argv[1], dtype='i', delimiter=' ')
 
 mshape = input.shape
 
-print(mshape)
-print(input)
+print(f' > Input shape: {mshape}')
+# print(input)
 
 # Add a border to the plot
 
 bordermat = np.zeros((mshape[0]+2, mshape[1]+2))
 bordermat[0, :] = 1
-bordermat[-1,:] = 1
+bordermat[-1, :] = 1
 bordermat[:, 0] = 1
-bordermat[:,-1] = 1
+bordermat[:, -1] = 1
 
 # And copy the other matrix within
 
-bordermat[1:-1,1:-1] = input[:,:]
+bordermat[1:-1, 1:-1] = input[:, :]
 
 plt.imsave(sys.argv[1]+".png", bordermat, cmap='gray_r')
